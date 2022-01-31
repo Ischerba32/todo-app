608b47ee4428f5a31b690342497683a9b9207434
Ischerba32 <i.scherba32@gmail.com>
2022-01-31 18:32:41 +0300

    removed dafault files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './redux/App';
-import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from "./redux/store";
 
@@ -15,4 +13,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-reportWebVitals();
